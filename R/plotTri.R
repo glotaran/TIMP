1d98f9502f0dbb008c08bb02ff6260979d4bdd49
Joris Snellenburg <jsnel@users.noreply.github.com>
2022-12-12 02:35:49 +0100

    🎨Reformat according to R-lib styler
    Using the `styler` package, ran from the R-studio add-in on the entire package.
    
    Autoformatting changes only.

--- R/plotTri.R
+++ R/plotTri.R
@@ -1,20 +1,26 @@
 "plotTri" <- function(xx) {
   oldpar <- par(no.readonly = TRUE)
   on.exit(par(oldpar))
-  if(dev.cur() != 1)
+  if (dev.cur() != 1) {
     dev.new()
-  par(mfrow=c(3, 1))
-  svddatalist <-doSVD(xx, 2, 2)
+  }
+  par(mfrow = c(3, 1))
+  svddatalist <- doSVD(xx, 2, 2)
 
-  matplot(svddatalist$left, type = "l",
-          main = "Left sing. vectors alpha",
-          log = "x")
+  matplot(svddatalist$left,
+    type = "l",
+    main = "Left sing. vectors alpha",
+    log = "x"
+  )
 
-  matplot(t(svddatalist$right), type = "l",
-          main = "Right sing. vectors alpha")
+  matplot(t(svddatalist$right),
+    type = "l",
+    main = "Right sing. vectors alpha"
+  )
 
   plot(1:length(svddatalist$values),
-       log10(svddatalist$values), xlab="",
-       main = "Sing. values alpha", type = "b")
-
+    log10(svddatalist$values),
+    xlab = "",
+    main = "Sing. values alpha", type = "b"
+  )
 }
